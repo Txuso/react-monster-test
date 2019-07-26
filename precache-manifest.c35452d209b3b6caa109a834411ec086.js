af9188316653cff6366b9d9f9917218205ded45c
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-07-26 13:44:08 +0200

    Updates

---
+++ precache-manifest.c35452d209b3b6caa109a834411ec086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ba31ff26cb5cd65370a2eb064909c9",
+    "url": "/react-monster-test/index.html"
+  },
+  {
+    "revision": "fcd923b93143d636bb02",
+    "url": "/react-monster-test/static/css/main.7debc030.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monster-test/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fcd923b93143d636bb02",
+    "url": "/react-monster-test/static/js/main.d6a7e7b8.chunk.js"
+  },
+  {
+    "revision": "4472b5c457375858ce08",
+    "url": "/react-monster-test/static/js/runtime~main.8e687eaf.js"
+  }
+]);
